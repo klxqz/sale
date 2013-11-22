86cd5714230a126a9a55ce7f5aa7d5dc18de9f28
klxqz <klxqz@mail.ru>
2013-11-22 14:01:32 +0600

    смена разработчика

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,17 +3,16 @@
 return array(
     'name' => 'Растродажа',
     'description' => 'Отдельная страница из товаров, у которые есть зачеркнутая цена',
-    'vendor'=>903438,
-    'version'=>'1.0.0',
-    'img'=>'img/sale.png',
+    'vendor' => '985310',
+    'version' => '1.0.1',
+    'img' => 'img/sale.png',
     'shop_settings' => true,
-    'frontend'    => true,
-    'icons'=>array(
-        16=>'img/sale.png',
+    'frontend' => true,
+    'icons' => array(
+        16 => 'img/sale.png',
     ),
     'handlers' => array(
         'frontend_nav' => 'frontendNav',
     ),
-
 );
 //EOF
