57fedc796e81aeb6c2fd838975d48e763e4f846a
klxqz <klxqz@mail.ru>
2013-12-09 16:38:09 +0600

    v1.0.2+

--- lib/classes/shopSaleProductsCollection.class.php
+++ lib/classes/shopSaleProductsCollection.class.php
@@ -1,17 +1,20 @@
 <?php
 
-class shopSaleProductsCollection extends shopProductsCollection
-{
-    public function saleFilter()
-    {
+class shopSaleProductsCollection extends shopProductsCollection {
+
+    public function saleFilter($rand = false) {
         if ($this->filtered) {
             return;
         }
 
         $this->where[] = 'p.compare_price > p.price';
-        
-        
+
+        if ($rand) {
+            $this->order_by = "RAND()";
+        }
+
+        $this->prepared = true;
         $this->filtered = true;
     }
-    
+
 }
--- lib/shopSale.plugin.php
+++ lib/shopSale.plugin.php
@@ -1,42 +1,41 @@
 <?php
-class shopSalePlugin extends shopPlugin
-{
 
-    public function frontendNav()
-    {
-        if($this->getSettings('default_output')) {
+class shopSalePlugin extends shopPlugin {
+
+    public function frontendNav() {
+        if ($this->getSettings('default_output')) {
             return self::display();
         }
     }
-    
-    public static function display()
-    {
+
+    public static function display() {
         $plugin_id = array('shop', 'sale');
         $tmp_path = 'plugins/sale/templates/Sale.html';
-        
-        $app_settings_model = new waAppSettingsModel();
-        $status = $app_settings_model->get($plugin_id,'status');
 
-        if(!$status) {
+        $app_settings_model = new waAppSettingsModel();
+        $status = $app_settings_model->get($plugin_id, 'status');
+
+        if (!$status) {
             return;
         }
 
         $count = $app_settings_model->get($plugin_id, 'count');
-        
+
         $collection = new shopSaleProductsCollection();
-        $collection->saleFilter();
+        $collection->saleFilter(true);
         $products = $collection->getProducts('*', 0, $count);
 
         $view = wa()->getView();
 
         $view->assign('sale_products', $products);
-        
+
         $template_path = wa()->getDataPath($tmp_path, false, 'shop', true);
-        if(!file_exists($template_path)) {
-            $template_path =wa()->getAppPath($tmp_path,  'shop');
+        if (!file_exists($template_path)) {
+            $template_path = wa()->getAppPath($tmp_path, 'shop');
         }
-        
-		$html = $view->fetch($template_path);
+
+        $html = $view->fetch($template_path);
         return $html;
     }
+
 }
